8d74e134541a50cc733a3d49b6efc0d6861fec1b
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-07-21 13:00:02 +0200

    fix: update turn servers

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -2,12 +2,15 @@
 
 const _DEFAULT_CONFIG = {
   'iceServers': [
-    {'urls': "stun:stun.l.google.com:19302"},
+    {'urls': "stun:stun.bethesda.net:3478"},
     {
-      'urls': "turn:0.peerjs.com:3478",
-      'username': "peerjs",
-      'credential': "peerjsp"
-    }
+      "urls": [
+        "turn:eu-0.turn.peerjs.com:3478",
+        "turn:us-0.turn.peerjs.com:3478",
+      ],
+      "username": "peerjs",
+      "credential": "peerjsp",
+    },
   ],
   'sdpSemantics': "unified-plan"
 };

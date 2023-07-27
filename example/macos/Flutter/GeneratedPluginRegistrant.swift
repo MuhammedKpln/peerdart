bf46401b1f20326499d6a5046ec3c1055b940895
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-07-27 21:56:42 +0200

    chore: remove platform files

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_webrtc
-import path_provider_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
